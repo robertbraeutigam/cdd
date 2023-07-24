0f28dce4705818b1cd012cf41dc59b837558051f
Robert Brautigam <robert.brautigam@prewave.ai>
2023-07-24 21:11:45 +0200

    Add separate method

--- src/main/java/dev/robertbrautigam/cdd/middleware/cashtransfer/CashTransferMiddlewareService.java
+++ src/main/java/dev/robertbrautigam/cdd/middleware/cashtransfer/CashTransferMiddlewareService.java
@@ -4,17 +4,23 @@ import dev.robertbrautigam.cdd.middleware.MiddlewareService;
 import dev.robertbrautigam.cdd.SystemException;
 import dev.robertbrautigam.cdd.backend.cashtransfer.*;
 
+import java.math.BigDecimal;
+
 public class CashTransferMiddlewareService implements MiddlewareService<CashTransferMiddlewareRequest, CashTransferMiddlewareResponse> {
    @Override
    public CashTransferMiddlewareResponse call(CashTransferMiddlewareRequest request) throws SystemException {
-      CashTransferBackendRequest backendRequest = new CashTransferBackendRequest();
-      backendRequest.setAmount(request.getAmount());
-      backendRequest.setTargetAccount(request.getTargetAccount());
-      CashTransferBackendResponse backendResponse = new CashTransferBackendService().call(backendRequest);
+      String transferId = submitCashTransfer(request.getAmount(), request.getTargetAccount());
 
       CashTransferMiddlewareResponse response = new CashTransferMiddlewareResponse();
-      response.setTransferId(backendResponse.getTransferId());
+      response.setTransferId(transferId);
       return response;
+   }
+
+   private String submitCashTransfer(BigDecimal amount, String targetAccount) throws SystemException {
+      CashTransferBackendRequest backendRequest = new CashTransferBackendRequest();
+      backendRequest.setAmount(amount);
+      backendRequest.setTargetAccount(targetAccount);
+      return new CashTransferBackendService().call(backendRequest).getTransferId();
    }
 }
 
